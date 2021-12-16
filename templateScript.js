4c6eef68f9072ad560b8ffd8fec4c6ba56114920
Dang Hoang Phong <71632629+phongdh228@users.noreply.github.com>
2021-12-16 14:27:32 +0700

    first template frame (not finished yet)

---
+++ templateScript.js
@@ -0,0 +1,23 @@
+function openMenu(evt, menuName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("menu");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+    }
+    document.getElementById(menuName).style.display = "block";
+    evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+  }
+  
+  document.getElementById("myLink").click();
+  
+  function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+   
+  function w3_close() {
+      document.getElementById("mySidebar").style.display = "none";
+  }
